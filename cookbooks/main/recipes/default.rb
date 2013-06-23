efd7d2cc08b53ca2721800776d8f02bf2a2e5513
Piotr Solnica <piotr.solnica@gmail.com>
2013-06-23 20:23:24 +0200

    Add git and most common rdbms dbs

---
+++ cookbooks/main/recipes/default.rb
@@ -0,0 +1,13 @@
+package 'git'
+
+# sqlite3
+package 'sqlite3'
+package 'libsqlite3-dev'
+
+# postgres
+package 'postgresql'
+package 'libpq-dev'
+
+# mysql
+package 'mysql-server'
+package 'libmysqlclient-dev'
